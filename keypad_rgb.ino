b90486d4538e2132be15de00a0f1a80caef1ee76
vijay1923 <vijaymagadum1923@gmail.com>
2026-04-20 19:09:42 +0530

    version 2.o

--- keypad_rgb.ino
+++ keypad_rgb.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Keypad.h>
 
-#define LED 23
+#define LED 2
 #define DEBOUNCE_TIME 200 
 
 char keys[4][3] =
@@ -13,8 +13,8 @@ char keys[4][3] =
 };
 
 
-byte pin_rows[4] = {32, 33, 25, 26}; 
-byte pin_column[3] = {21,18,19};
+byte pin_rows[4]={32,33,25,26}; 
+byte pin_column[3]={21,18,19};
 
 
 // byte pin_rows[4] = {21,19,18,32};    // for 4 * 4
@@ -32,13 +32,14 @@ bool keyHeld = false;
 void setup()
 {
     Serial.begin(115200);
-   // delay(100); 
-    Serial1.begin(9600, SERIAL_8N1, 0, 4);  // for TX/RX pins (GPIO4 for TX, GPIO0 for RX)
+    delay(100); 
+       Serial1.begin(9600, SERIAL_8N1, 4, 0);  // new 18/04/2026
+    // Serial1.begin(9600, SERIAL_8N1, 0, 4);  // for TX/RX pins 
    //  Serial1.begin(9600, SERIAL_8N1, 16, 17);  // for TX/RX pins (GPIO16 for TX, GPIO17 for RX)
   //  delay(100); 
     pinMode(LED, OUTPUT);
     Serial.println("Welcome ESP32 Dev Module : Serial Keypad");
-    Serial1.println("Welcome ESP32 Dev Module : Serial RS232");
+    Serial1.println("\nWelcome serial keypad");  // Send welcome message to Serial1
 }
 
 void loop()
@@ -64,12 +65,12 @@ void loop()
                 Serial.println(")");
             }
 
-            if (key == 0x0A)
-            {
-                Serial.println("\nRestarting ESP32\n");
-              //  delay(300);
-                ESP.restart();
-            }
+            // if (key == 0x0A)
+            // {
+            //     Serial.println("\nRestarting ESP32\n");
+            //     delay(300);
+            //     ESP.restart();
+            // }
         }
     }
     else if (state == RELEASED)
